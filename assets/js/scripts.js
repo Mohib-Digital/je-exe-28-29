cc3cfac1e581dbc819234d1793efccde58ae883b
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-07-28 09:24:52 -0700

    add codes

---
+++ assets/js/scripts.js
@@ -0,0 +1,64 @@
+
+
+// exercise of chapter 28 and 29
+
+// program 01
+// var myStirng = "123abc";
+// var inOrignalNo = parseInt(myStirng);
+// console.log(inOrignalNo)
+// console.log(typeof inOrignalNo);
+
+
+// program 02
+// var myStirng = "45.67xyz";
+// var inOrignalNo = parseFloat(myStirng);
+// console.log(inOrignalNo);
+// console.log(typeof inOrignalNo);
+
+
+// program 03
+// var myStirng = "123.45";
+// var inOrignalNo = Number(myStirng);
+// console.log(inOrignalNo);
+// console.log(typeof inOrignalNo);
+
+
+// progarm 04
+// var myStirng = 789;
+// var convertedToString = myStirng.toString();
+// console.log(convertedToString) 
+// console.log(typeof convertedToString)
+
+
+// program 05
+// var myStirng = "256px";
+// var inOrignalNo = parseInt(myStirng);
+// console.log(inOrignalNo);
+// console.log(typeof inOrignalNo);
+
+// we get the value in original number (256), now we are converting the value to string
+var num = 256;
+var convertedToString = num.toString();
+console.log(convertedToString);
+console.log(typeof convertedToString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
